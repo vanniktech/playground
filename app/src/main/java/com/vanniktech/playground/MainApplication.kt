c0121046b663dcdd43a0ffcbadf4bcf3316a5618
Niklas Baudy <niklas.baudy@vanniktech.de>
2020-08-12 07:26:56 +0200

    Add AndroidThreeTen.

--- app/src/main/java/com/vanniktech/playground/MainApplication.kt
+++ app/src/main/java/com/vanniktech/playground/MainApplication.kt
@@ -1,9 +1,11 @@
 package com.vanniktech.playground
 
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 
 class MainApplication : Application() {
   override fun onCreate() {
     super.onCreate()
+    AndroidThreeTen.init(this)
   }
 }

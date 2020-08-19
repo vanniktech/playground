e9a8f3693eeed10e90d4034d4cebc220cf4ef99f
Niklas Baudy <niklas.baudy@vanniktech.de>
2020-08-19 06:36:34 +0200

    Add Timber.

--- app/src/main/java/com/vanniktech/playground/MainApplication.kt
+++ app/src/main/java/com/vanniktech/playground/MainApplication.kt
@@ -2,10 +2,12 @@ package com.vanniktech.playground
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import timber.log.Timber
 
 class MainApplication : Application() {
   override fun onCreate() {
     super.onCreate()
     AndroidThreeTen.init(this)
+    Timber.plant(Timber.DebugTree())
   }
 }

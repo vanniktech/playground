4b44ea3ad9d7073ec1c2dd7acc64eac0872d7a47
Niklas Baudy <niklas.baudy@vanniktech.de>
2020-09-01 21:12:06 +0200

    Use ViewBinding.

--- app/src/main/java/com/vanniktech/playground/MainActivity.kt
+++ app/src/main/java/com/vanniktech/playground/MainActivity.kt
@@ -2,10 +2,13 @@ package com.vanniktech.playground
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.vanniktech.playground.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+
+    val binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
   }
 }

da58375d0a371cf08efd0ae5eceafdc22a62617d
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-05-24 11:42:52 +0200

    Repro case for KTIJ-21811: Databinding unresolved reference with Android Gradle Plugin 7.0.4

--- app/src/main/java/com/vanniktech/playground/MainActivity.kt
+++ app/src/main/java/com/vanniktech/playground/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
+    // ActivityMainBinding can't be resolved. This is due to setting android.namespace in the Gradle Block as well as removing the package from the Manifest file.
     val binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
   }

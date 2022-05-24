33aac108bc5608010164f1a6feb84676620cb556
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-05-24 14:41:05 +0200

    KMP module. (#5)

---
+++ kmp/src/commonMain/kotlin/com/vanniktech/playground/kmp/KotlinDateTime.kt
@@ -0,0 +1,9 @@
+package com.vanniktech.playground.kmp
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+
+/** Clock. */
+fun Clock.localDateTime() = now().toLocalDateTime(TimeZone.currentSystemDefault())
+fun Clock.localDate() = localDateTime().date

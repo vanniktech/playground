f96758a867a110d7b13aeba9672f1596ea12a90c
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-10-07 14:57:42 +0200

    Reproducer for: KTIJ-23167 - Weird breakpoint options

--- app/src/main/kotlin/com/vanniktech/playground/MainActivity.kt
+++ app/src/main/kotlin/com/vanniktech/playground/MainActivity.kt
@@ -3,12 +3,41 @@ package com.vanniktech.playground
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.vanniktech.playground.databinding.ActivityMainBinding
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 
 class MainActivity : AppCompatActivity() {
+  private val compositeDisposable = CompositeDisposable()
+  private var alreadySeen = mutableSetOf<String>()
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     val binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+  }
+
+  private fun mark(position: Int) {
+    if (position >= 0) {
+      val item = peek(position)
+
+      if (item != null && alreadySeen.add(item.itemId) && item.read == null) {
+        compositeDisposable += Single.just(5)
+          .subscribe({ }, { })
+      }
+    }
+  }
+
+  private fun peek(position: Int): Item? = null
+}
+
+data class Item(
+  val itemId: String,
+  val read: Date?,
+)
+
+operator fun CompositeDisposable.plusAssign(disposable: Disposable?) {
+  if (disposable != null) {
+    add(disposable)
   }
 }

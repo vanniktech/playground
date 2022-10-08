cdbbb4bb945e66f280a982c4da0baf5674d6b35c
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-10-08 11:58:02 +0200

    Kotlin: Enter yields wrong indentation.

--- kotlin/src/main/kotlin/com/vanniktech/playground/kotlin/Main.kt
+++ kotlin/src/main/kotlin/com/vanniktech/playground/kotlin/Main.kt
@@ -2,4 +2,16 @@ package com.vanniktech.playground.kotlin
 
 fun main() {
   print("Hello World")
+
+  foo(
+    a = {
+      foo(
+        a = {},
+      )
+    }
+  )
+}
+
+fun foo(a: () -> Unit) {
+
 }

e14be86d224b6e18329484c0827f41f9c2775931
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-10-11 12:32:49 +0200

    Reproducer for: IDEA-303528 - Jump to reference of parameter in function prefers extension function over parameter.

--- kotlin/src/main/kotlin/com/vanniktech/playground/kotlin/Main.kt
+++ kotlin/src/main/kotlin/com/vanniktech/playground/kotlin/Main.kt
@@ -3,3 +3,13 @@ package com.vanniktech.playground.kotlin
 fun main() {
   print("Hello World")
 }
+
+fun myFunction(
+  foo: Int,
+  /** [bar] is different from [foo]. */
+  bar: Int,
+) {
+
+}
+
+fun Int.foo() = 5

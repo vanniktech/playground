c841c71c91d28eae0d7c3f1b52670e0d7c7c64e8
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-10-17 12:12:03 +0200

    IDEA-303949: Inconsistent Find Usage on constructor call site.

--- kotlin/src/main/kotlin/com/vanniktech/playground/kotlin/Main.kt
+++ kotlin/src/main/kotlin/com/vanniktech/playground/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package com.vanniktech.playground.kotlin
 
 fun main() {
-  print("Hello World")
+  val car = Car(4)
+  echo(car)
 }
+
+fun echo(car: Car) {
+  print(car)
+}
+
+data class Car(val wheels: Int)

f6da62022c6c14d55a617da17eeb2e1f3196cb36
Niklas Baudy <niklas.baudy@vanniktech.de>
2023-04-18 12:53:04 +0200

    Repro for OkHttp #7381 - IllegalStateException: Unbalanced enter/exit.

--- kotlin/src/main/kotlin/com/vanniktech/playground/kotlin/Main.kt
+++ kotlin/src/main/kotlin/com/vanniktech/playground/kotlin/Main.kt
@@ -1,5 +1,32 @@
 package com.vanniktech.playground.kotlin
 
+import okhttp3.OkHttpClient
+import okhttp3.Request.Builder
+import okhttp3.Response
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import java.net.ProtocolException
+
 fun main() {
-  print("Hello World")
+  WebSocketAbstraction(OkHttpClient.Builder().build())
+}
+
+class WebSocketAbstraction(
+  private val okHttpClient: OkHttpClient,
+) : WebSocketListener() {
+  private fun create() = okHttpClient.newBuilder()
+    .build()
+    .newWebSocket(Builder().url("https://api-dev.becoach.app/websocket/coach/chat?163b198b-08ab-487e-b5b1-0648289ac0f6").build(), this)
+
+  init {
+    create()
+  }
+
+  override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+    if (t is ProtocolException && t.message == "Expected HTTP 101 response but was '401 '") {
+      create()
+    } else {
+      t.printStackTrace()
+    }
+  }
 }

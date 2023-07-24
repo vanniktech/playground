92521691bf2198d634fd6ca3041c195dcc82fddf
Niklas Baudy <niklas.baudy@vanniktech.de>
2023-07-24 12:04:02 +0100

    Reproducer for Multiplatform Settings serializedValue.

--- kmp/src/commonTest/kotlin/com/vanniktech/playground/kmp/SomeTest.kt
+++ kmp/src/commonTest/kotlin/com/vanniktech/playground/kmp/SomeTest.kt
@@ -1,10 +1,31 @@
 package com.vanniktech.playground.kmp
 
+import com.russhwolf.settings.ExperimentalSettingsApi
+import com.russhwolf.settings.MapSettings
+import com.russhwolf.settings.Settings
+import com.russhwolf.settings.serialization.serializedValue
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class SomeTest {
   @Test fun foo() {
-    assertEquals(expected = 4, actual = 4)
+    val preferences = Preferences(MapSettings())
+    assertEquals(expected = emptyList(), actual = preferences.list)
+
+    val list = listOf("Hello", "World")
+    preferences.list = list
+    assertEquals(expected = list, actual = preferences.list)
+  }
+
+  class Preferences(settings: Settings) {
+    @OptIn(ExperimentalSerializationApi::class, ExperimentalSettingsApi::class)
+    var list by settings.serializedValue(
+      serializer = ListSerializer(String.serializer()),
+      key = "something",
+      defaultValue = emptyList(),
+    )
   }
 }

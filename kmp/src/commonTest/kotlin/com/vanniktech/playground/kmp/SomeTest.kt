af6e0ba49fb298e099c83f78be7c42d6ab01c668
Niklas Baudy <niklas.baudy@vanniktech.de>
2023-08-19 13:14:58 +0100

    Reproducer for Ktor incorrect etag.

--- kmp/src/commonTest/kotlin/com/vanniktech/playground/kmp/SomeTest.kt
+++ kmp/src/commonTest/kotlin/com/vanniktech/playground/kmp/SomeTest.kt
@@ -1,10 +1,15 @@
 package com.vanniktech.playground.kmp
 
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.http.HttpHeaders
+import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class SomeTest {
-  @Test fun foo() {
-    assertEquals(expected = 4, actual = 4)
+  @Test fun foo() = runBlocking {
+    val response = HttpClient().get("https://lexfridman.com/feed/podcast")
+    assertEquals("36d4b983b1bf2e278de73dd388cbb77c", actual = response.headers[HttpHeaders.ETag])
   }
 }

ee9eec2cdad1d6c0deace8de4645edfec56bc9b0
Niklas Baudy <niklas.baudy@vanniktech.de>
2023-08-19 13:55:12 +0100

    Reproducer for IntelliJ broken Go to Test in Enums.

---
+++ kmp/src/commonMain/kotlin/com/vanniktech/playground/kmp/MyEnum.kt
@@ -0,0 +1,8 @@
+package com.vanniktech.playground.kmp
+
+enum class MyEnum(
+  val value: Int? = null,
+) {
+  FOO(value = 5),
+  Foo,
+}
---
+++ kmp/src/commonTest/kotlin/com/vanniktech/playground/kmp/MyEnumTest.kt
@@ -0,0 +1,10 @@
+package com.vanniktech.playground.kmp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MyEnumTest {
+  @Test fun foo() {
+    assertEquals(expected = 4, actual = 4)
+  }
+}

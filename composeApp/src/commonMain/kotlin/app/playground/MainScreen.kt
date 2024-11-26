f1661de75ea2e3e696f79d23b4ec54aa16ec37ab
Niklas Baudy <niklas.baudy@vanniktech.de>
2024-11-26 09:37:19 +0700

    Reproducer for Customize RangeSlider colors.

--- composeApp/src/commonMain/kotlin/app/playground/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/app/playground/MainScreen.kt
@@ -1,12 +1,104 @@
 package app.playground
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Label
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.RangeSliderState
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable internal fun MainScreen(
   navHostController: NavHostController,
+  currentMin: Int = 10,
+  currentMax: Int = 50,
+  range: IntRange = 1..100,
 ) = Column {
   Text("Hello World")
+
+  val rangeSliderState = remember {
+    RangeSliderState(
+      currentMin.toFloat(),
+      currentMax.toFloat(),
+      steps = range.last - range.first,
+      valueRange = range.first.toFloat()..range.last.toFloat(),
+    )
+  }
+
+  rangeSliderState.onValueChangeFinished = {
+    // onChanged(rangeSliderState.activeRangeStart.toInt(), rangeSliderState.activeRangeEnd.toInt())
+  }
+
+  val startInteractionSource = remember { MutableInteractionSource() }
+  val endInteractionSource = remember { MutableInteractionSource() }
+
+  Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)) {
+    RangeSlider(
+      state = rangeSliderState,
+      modifier = Modifier.semantics { contentDescription = "Foo" },
+      startInteractionSource = startInteractionSource,
+      endInteractionSource = endInteractionSource,
+      colors = SliderDefaults.colors(),
+      startThumb = {
+        Label(
+          label = {
+            PlainTooltip(modifier = Modifier.sizeIn(45.dp, 25.dp).wrapContentWidth()) {
+              Text(
+                text = rangeSliderState.activeRangeStart.toInt().toString(),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+              )
+            }
+          },
+          interactionSource = startInteractionSource,
+        ) {
+          SliderDefaults.Thumb(
+            interactionSource = startInteractionSource,
+          )
+        }
+      },
+      endThumb = {
+        Label(
+          label = {
+            PlainTooltip(modifier = Modifier.sizeIn(45.dp, 25.dp).wrapContentWidth()) {
+              Text(
+                text = rangeSliderState.activeRangeEnd.toInt().toString(),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+              )
+            }
+          },
+          interactionSource = endInteractionSource,
+        ) {
+          SliderDefaults.Thumb(
+            interactionSource = endInteractionSource,
+          )
+        }
+      },
+      track = { rangeSliderState ->
+        SliderDefaults.Track(
+          rangeSliderState = rangeSliderState,
+          drawStopIndicator = null,
+          drawTick = { _, _ -> },
+        )
+      },
+    )
+  }
 }
--- composeApp/src/commonMain/kotlin/app/playground/Theme.kt
+++ composeApp/src/commonMain/kotlin/app/playground/Theme.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 @Composable fun Theme(
   darkTheme: Boolean = isSystemInDarkTheme(),
@@ -22,8 +23,15 @@ import androidx.compose.runtime.Composable
   )
 }
 
-private val LightColors = lightColorScheme()
+val color = Color.Red
+private val LightColors = lightColorScheme(
+  primary = color,
+  secondaryContainer = color,
+)
 
-private val DarkColors = darkColorScheme()
+private val DarkColors = darkColorScheme(
+  primary = color,
+  secondaryContainer = color,
+)
 
 private val Typography = Typography()

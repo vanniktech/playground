686984947b1e3308b67451c12b380be1ce9f4293
Niklas Baudy <niklas.baudy@vanniktech.de>
2024-12-12 16:40:50 +0800

    Reproducer for 'Nested navigation use String.route with type safety crashes'

--- composeApp/src/commonMain/kotlin/app/playground/DeepLink.kt
+++ composeApp/src/commonMain/kotlin/app/playground/DeepLink.kt
@@ -4,3 +4,9 @@ import kotlinx.serialization.Serializable
 
 sealed interface DeepLink
 @Serializable data object DeepLinkMain : DeepLink
+@Serializable data object DeepLinkOnboarding : DeepLink
+
+enum class DeepLinkOnboardingStep {
+  NAME,
+  BIRTHDAY,
+}

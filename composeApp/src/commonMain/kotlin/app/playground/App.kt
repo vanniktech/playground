b8b2b65964308d190d90c6034a2d09588a1cf3fc
Niklas Baudy <niklas.baudy@vanniktech.de>
2024-12-13 21:11:24 +0800

    Slack Compose Lints.

--- composeApp/src/commonMain/kotlin/app/playground/App.kt
+++ composeApp/src/commonMain/kotlin/app/playground/App.kt
@@ -1,6 +1,8 @@
 package app.playground
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Preview
@@ -11,3 +13,11 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
     frontendDependencies = frontendDependencies,
   )
 }
+
+@Composable
+private fun MyComposable() {
+  val viewModel = viewModel<MyViewModel>()
+  // ...
+}
+
+class MyViewModel : ViewModel()

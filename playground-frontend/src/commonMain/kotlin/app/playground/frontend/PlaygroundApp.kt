50d9bf72823f41b1cd673828b9226ba75522cbf6
Niklas Baudy <niklas.baudy@vanniktech.de>
2026-02-22 16:09:25 -0600

    Reproducer for Material Scaffold does not apply proper insets in case a FloatingActionButton is added.

--- playground-frontend/src/commonMain/kotlin/app/playground/frontend/PlaygroundApp.kt
+++ playground-frontend/src/commonMain/kotlin/app/playground/frontend/PlaygroundApp.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -26,6 +27,7 @@ import app.playground.frontend.modal.entryProviderModal
 ) = Theme {
   initCoil(dependencies)
   val backStack = rememberNavBackStack(NavKeyConfig, RouteMain)
+  val currentRoute = backStack.lastOrNull()
 
   Scaffold(
     topBar = {
@@ -40,6 +42,14 @@ import app.playground.frontend.modal.entryProviderModal
         ),
       )
     },
+    floatingActionButton = {
+      when (currentRoute) {
+        is RouteMain -> ExtendedFloatingActionButton(onClick = { }) {
+          Text("Foo")
+        }
+        else -> Unit
+      }
+    }
   ) { innerPadding ->
     NavDisplay(
       backStack = backStack,
